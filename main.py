a8f6b9884796bc57a8d36dd93870ea7d00fb2942
bakerc12 <143834496+bakerc12@users.noreply.github.com>
2023-10-25 09:39:38 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# These are my two recursive functions. The first on is a greatest common divisor,
+# which uses the euclidean algorithm to find the highest common factor between 2 numbers or the greatest common divisor
+'''def gcd (x, y):
+    if (y == 0):
+        return x
+    else:
+        return gcd (y, x % y)
+x = 48
+y = 60
+print ("the GCD of ",x, "and" ,y," is ", end="")
+print(gcd(60,48)'''
+
+#my second function is used to find the factorial of a positive integer.
+# it works by taking a number, then multiplying it by the number below it. this cycle continues until it is multiplied by 1
+'''def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+result = factorial(5)
+print (result)'''
+
